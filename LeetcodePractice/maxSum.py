ff3088c4d449b9f3fbef66dde15eb94e13f05417
RIKI SAHA <106177772+NIKKISAHA@users.noreply.github.com>
2023-08-16 22:00:37 +0530

    Update maxSum.py

--- LeetcodePractice/maxSum.py
+++ LeetcodePractice/maxSum.py
@@ -1,4 +1,5 @@
 #MaxSum
+#MaxSum
 class Ri():
     def ri(self,x:list[int])->int:
         maxnum=x[0]
@@ -9,15 +10,18 @@ class Ri():
             m+=x[i]
             maxnum=max(m,maxnum)
         return maxnum
+        m=0
 
         # nested loop
         maxnum=x[0]
-        c=0
+        
         for i in range(len(x)):
+            c=x[i]
             for j in range(i+1,len(x)):
-                c=x[j]+x[j]
+                print(c)
+                c+=x[j]
                 maxnum=max(c,maxnum)
-                print(maxnum)
+                # print(maxnum)
         return maxnum
 riki=Ri()
-print(riki.ri([-4,-2,0]))
+print(riki.ri([2,-8,0]))
