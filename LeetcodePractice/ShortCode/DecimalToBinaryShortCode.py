f19ea60e5014e4ae7e3ac2e20f7d173302b4ca57
RIKI SAHA <106177772+NIKKISAHA@users.noreply.github.com>
2023-09-20 22:29:53 +0530

    Delete LeetcodePractice/ShortCode/DecimalToBinaryShortCode.py

--- LeetcodePractice/ShortCode/DecimalToBinaryShortCode.py
+++
@@ -1,9 +0,0 @@
-#decimal to binary
-class Ri():
-    def ri(self,x:int,s:str="")->int:
-        if x==0:
-            return s
-        s+=str(x%2)
-        return self.ri(x//2,s)
-riki=Ri()
-print(riki.ri(3))
